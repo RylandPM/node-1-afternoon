0a9917f9988ca3cfad9fbd153a11017014258562
Ryland McGee <mcgee.reilly@gmail.com>
2019-05-10 14:34:49 -0800

    Complete

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const getProducts = require("./getProducts");
+const getProduct = require("./getProduct");
+const app = express();
+
+app.get("/api/products", getProducts);
+
+app.get("/api/products/:id", getProduct);
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
